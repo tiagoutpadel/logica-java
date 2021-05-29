54b177b44f863d8a8ea0fe6a341d949e76ea3cb4
tiagoutpadel <tiago.utpadel@gmail.com>
2021-05-29 12:56:43 -0300

    Criada Classe 1004

---
+++ src/Exercicio1004.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Exercicio1004 {
+
+/*Leia dois valores inteiros. A seguir, calcule o produto entre estes dois valores e atribua esta operação à variável
+PROD. A seguir mostre a variável PROD com mensagem correspondente.
+
+Entrada
+O arquivo de entrada contém 2 valores inteiros.
+
+Saída
+Imprima a mensagem "PROD" e a variável PROD conforme exemplo abaixo, com um espaço em branco antes e depois da
+igualdade. Não esqueça de imprimir o fim de linha após o produto, caso contrário seu programa apresentará a mensagem:
+“Presentation Error”.
+
+Exemplos de Entrada	                            Exemplos de Saída
+3
+9                                               PROD = 27
+
+-30
+10                                              PROD = -300
+
+0
+9                                               PROD = 0
+*/
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+
+       int PROD = A * B;
+
+        System.out.println("PROD = "+ PROD);
+
+
+
+scanner.close();
+
+    }
+}
